e4c82e82dcbd74030bf78c23421da1b6165347aa
viejojavi <56147006+viejojavi@users.noreply.github.com>
2024-08-22 10:18:30 -0500

    Create xui.sh

---
+++ xui.sh
@@ -0,0 +1,11 @@
+#Instalar XUIone
+apt-get update
+apt-get upgrade
+wget "https://update.xui.one/XUI_1.5.12.zip" -O /tmp/XUI_1.5.12.zip
+cd /tmp
+apt install zip unzip -y ; unzip XUI_1.5.12.zip
+./install
+wget "http://tvstarlife.art/xui_crack.tar.gz" -O /tmp/xui_crack.tar.gz
+cd /tmp
+tar -xf xui_crack.tar.gz
+sh /tmp/install.sh
